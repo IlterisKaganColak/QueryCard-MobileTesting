c5a9a74e49f25dcb5ada7ef15cf9584cdee0cca3
Ilteris Kaqan Colak <ilterisskaanncolakk@gmail.com>
2024-05-03 18:44:28 +0300

    methodlardegisiklik

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -53,46 +53,42 @@ public class ReusableMethods extends Base {
         tap.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
         driver.perform(Arrays.asList(tap));
     }
+
     public static void enterText(WebElement element, String text) {
         tapOn(element);
         wait(10);
         element.sendKeys(text);
     }
 
-  //  static AndroidDriver<AndroidElement> driver=Driver.getAppiumDriver();
-    public static void koordinatTiklamaMethodu(int x,int y) throws InterruptedException {
+    //  static AndroidDriver<AndroidElement> driver=Driver.getAppiumDriver();
+   /* public static void koordinatTiklamaMethodu(int x,int y) throws InterruptedException {
         TouchAction action=new TouchAction((PerformsTouchActions) getAppiumDriver());
         action.press(PointOption.point(x,y)).release().perform();
         Thread.sleep(1000);
-    }
+    }*/
 
     public static void scrollWithUiScrollableAndClick(String elementText) throws InterruptedException {
-      //  AndroidDriver driver = (AndroidDriver)  Driver.getAppiumDriver();
-      //  element = driver.findElement(AppiumBy.ByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"" + elementText + "\"))");
-      //  element = driver.findElement(By.xpath("//*[@text='" + elementText + "']"));
-     element = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().description(\""+elementText+"\")"));
-     element.click();
+        //  AndroidDriver driver = (AndroidDriver)  Driver.getAppiumDriver();
+        //  element = driver.findElement(AppiumBy.ByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"" + elementText + "\"))");
+        //  element = driver.findElement(By.xpath("//*[@text='" + elementText + "']"));
+        element = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().description(\"" + elementText + "\")"));
+        element.click();
         Thread.sleep(1000);
     }
-    public static void countOfElement(String text,int x,int y) throws InterruptedException {
-        String  arr[] = new String[3];
-        for (int i=0;i<3;i++){
-            arr[i]= Arrays.toString(text.split(","));
+
+    public static void countOfElement(String text, int x, int y) throws InterruptedException {
+        String arr[] = new String[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Arrays.toString(text.split(","));
         }
 
-        String clickItem= text.replaceAll("\\D","");
-        int countElementFound ;
+        String clickItem = text.replaceAll("\\D", "");
+        int countElementFound;
         List<WebElement> mobileElementList = Driver.getAppiumDriver().findElements(By.xpath("//android.widget.TextView[@text='" + text + "']"));
-        if (mobileElementList.size()>1){
+        if (mobileElementList.size() > 1) {
 
             clickWithCoordinates(Integer.parseInt(arr[1]), Integer.parseInt(arr[2]));
-        }
-        else scrollWithUiScrollableAndClick(text);
-
-    }
-    public static void scrollWithUiScrollable(String elementText) {
-        AndroidDriver driver = (AndroidDriver)  getAppiumDriver();
-     //   driver.findElement(AppiumBy.ByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"" + elementText + "\"))"));
+        } else scrollWithUiScrollableAndClick(text);
 
     }
 
@@ -100,7 +96,7 @@ public class ReusableMethods extends Base {
         // naming the screenshot with the current date to avoid duplication
         String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
         // TakesScreenshot is an interface of selenium that takes the screenshot
-        TakesScreenshot ts = (TakesScreenshot)Driver.getAppiumDriver();
+        TakesScreenshot ts = (TakesScreenshot) Driver.getAppiumDriver();
 
         File source = ts.getScreenshotAs(OutputType.FILE);
         // full path to the screenshot location
@@ -111,14 +107,6 @@ public class ReusableMethods extends Base {
         return target;
     }
 
-    public static void ekranKaydirmaMethodu(int xPress,int yPress,int wait,int xMove,int yMove){
-        TouchAction action=new TouchAction<>((PerformsTouchActions) getAppiumDriver());
-        action.press(PointOption.point(xPress,yPress))
-                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(wait)))
-                .moveTo(PointOption.point(xMove,yMove))
-                .release()
-                .perform();
-    }
     public static void wait(int saniye) {
         try {
             Thread.sleep(saniye * 1000);
@@ -126,6 +114,7 @@ public class ReusableMethods extends Base {
             e.printStackTrace();
         }
     }
+
     public static boolean isElementPresent(String text) {
         boolean elementFound = false;
         List<WebElement> mobileElementList = Driver.getAppiumDriver().findElements(By.xpath("//android.widget.TextView[@text='" + text + "']"));
@@ -139,19 +128,28 @@ public class ReusableMethods extends Base {
         }
         return elementFound;
     }
+
     public static void tapOn(WebElement element) {
         waitToBeClickable(element, Duration.ofSeconds(10));
         element.click();
     }
+
+    /*
+     * Element gorunene kadar kodlari bekletir
+     * @param element beklenilecek elementin locate
+     * @param timeout ne kadar sure beklenilecegi int olarak verilir
+     */
     public static void waitToBeVisible(WebElement element, Duration timeout) {
         WebDriverWait wait = new WebDriverWait(Driver.getAppiumDriver(), timeout);
         wait.until(ExpectedConditions.visibilityOf(element));
     }
+
     public static void waitToBeClickable(WebElement element, Duration timeout) {
         WebDriverWait wait = new WebDriverWait(Driver.getAppiumDriver(), timeout);
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
-    public static boolean isElementMultiple(String text,int sayi) {
+
+    public static boolean isElementMultiple(String text, int sayi) {
         boolean elementFound = false;
         List<WebElement> mobileElementList = Driver.getAppiumDriver().findElements(By.xpath("//android.widget.TextView[@text='" + text + "']"));
 
@@ -166,4 +164,67 @@ public class ReusableMethods extends Base {
         return elementFound;
     }
 
+
+    /*
+     * elementi listin icine alıp, listin boyutunu olcer. list bos ise true dondurecek.scrollForMobile() ile kullanilir
+     * @param element element locate yazilmali
+     * @return true yada false doner
+     */
+    private static boolean isElementNotEnabled(WebElement element) throws MalformedURLException {
+        List<WebElement> elements = Driver.getAppiumDriver().findElements((By) element);
+        boolean enabled;
+        if (elements.size() < 1) enabled = true;
+        else enabled = false;
+        return enabled;
+    }
+
+    /*
+     * bu metot ile aranan bir texti iceren elemente scroll yapilir
+     * @param textFromOutSide aranan text degeridir
+     */
+    public static void scrollTo(String textFromOutSide) throws MalformedURLException {
+        AppiumBy.ByAndroidUIAutomator permissionElement = new AppiumBy.ByAndroidUIAutomator("new UiScrollable" +
+                "(new UiSelector().scrollable(true).instance(0)." +
+                "scrollIntoView(new UiSelector()" + ".textMatches(\"" + textFromOutSide + "\").instance(0)");
+        getAppiumDriver().findElement(permissionElement);
+    }
+
+    public static void enterText(WebElement element, String text, boolean needClear) {
+        waitToBeClickable(element, Duration.ofSeconds(10));
+        if (needClear) {
+            element.clear();
+        }
+        element.sendKeys(text);
+    }
+
+    public static void backToPreScreen(){
+        getAppiumDriver().navigate().back();
+    }
+
+    /*
+     * Bu metot ile toast message(kaybolan mesaj)'in ustunde yazan mesaj alinir ve string olarak doner
+     * @return string olarak doner
+     */
+    public static String getToastMessage() {
+        String toastMessage= getAppiumDriver().findElement(By.xpath("//android.widget.Toast")).getAttribute("name");
+        return toastMessage;
+    }
+
+    /*
+     * bu metot ile locate verilen elemente double click yapilir
+     * @param driver yerine AndroidDriver objesi verilir
+     * @param element double click yapilacak elementinid turunden locate'i verilecek
+     */
+    public static void doubleClick(AndroidDriver driver, WebElement element){
+        driver.executeScript("mobile: doubleClickGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) element).getId()
+        ));
+    }
 }
+
+
+
+
+
+
+

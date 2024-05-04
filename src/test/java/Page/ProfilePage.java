f4de4f9ae27955505c741be778d688157dfac120
s-atalay <simgeeokurlar@gmail.com>
2024-05-05 00:06:32 +0300

    4.5.2024/7

--- src/test/java/Page/ProfilePage.java
+++ src/test/java/Page/ProfilePage.java
@@ -4,7 +4,6 @@ import hooks.Base;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import utilities.ConfigReader;
